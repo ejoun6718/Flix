d43448b5afc27d56490a7a692d750ebb9d3a6b43
ejoun6718 <hjoun@ucsd.edu>
2018-02-06 17:44:48 -0800

    Finish superhero tab

---
+++ Flix/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  Flix
+//
+//  Created by Hye Lim Joun on 2/6/18.
+//  Copyright © 2018 hyelim. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+  @IBOutlet weak var posterImageView: UIImageView!
+}
