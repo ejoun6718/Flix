8518072eb002654ce61cb4f267c5a8d279fd938f
ejoun6718 <hjoun@ucsd.edu>
2018-02-28 21:12:57 -0800

    Finish step 2 of milestone 4

--- Flix/NowPlayingViewController.swift
+++ Flix/NowPlayingViewController.swift
@@ -47,25 +47,13 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
   }
   
   func fetchMovies() {
-    let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
-    let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
-    let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
-    let task = session.dataTask(with: request) { (data, response, error) in
-      // This will run when the network request returns
-      if let error = error {
-        print(error.localizedDescription)
-      } else if let data = data {
-        let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
-        let movieDictionaries = dataDictionary["results"] as! [[String: Any]]
-
-        self.movies = Movie.movies(dictionaries: movieDictionaries)
-        
-        // Wait for network request
+    MovieApiManager().nowPlayingMovies { (movies: [Movie]?, error: Error?) in
+      if let movies = movies {
+        self.movies = movies
         self.tableView.reloadData()
-        self.refreshControl.endRefreshing()
       }
     }
-    task.resume()
+    self.refreshControl.endRefreshing()
   }
   
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
--- Flix/SuperheroViewController.swift
+++ Flix/SuperheroViewController.swift
@@ -32,24 +32,11 @@ class SuperheroViewController: UIViewController, UICollectionViewDataSource {
   }
   
   func fetchMovies() {
-    let url = URL(string: "https://api.themoviedb.org/3/movie/284053/similar?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed&language=en-US&page=1")!
-    let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
-    let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
-    let task = session.dataTask(with: request) { (data, response, error) in
-      // This will run when the network request returns
-      if let error = error {
-        print(error.localizedDescription)
-      } else if let data = data {
-        let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
-        let movieDictionaries = dataDictionary["results"] as! [[String: Any]]
-        
-        self.movies = Movie.movies(dictionaries: movieDictionaries)
-        
-        // Wait for network request
-        self.collectionView.reloadData()
+    MovieApiManager().nowPlayingMovies { (movies: [Movie]?, error: Error?) in
+      if let movies = movies {
+        self.movies = movies
       }
     }
-    task.resume()
   }
   
   override func didReceiveMemoryWarning() {
